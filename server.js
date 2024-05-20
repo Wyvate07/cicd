28db1f15f21d411a0b46c8a966231fd2f27443b2
Wyvate07 <165587133+Wyvate07@users.noreply.github.com>
2024-05-20 16:55:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send(`Hello World, Hope you are doing well !! ${process.env.USER_NAME}`);
+  res.send(`Hello World, Hope you are doing well !! ${process.env.USER_NAME} ${process.env.PROD_PYTHON_BACKEND_CASHFREE_PUBLIC_KEY}`);
 });
 
 app.listen(PORT, HOST);
